5ae3e0a8ae8a6cca9e48e7189a51b96a0f40fa4c
IntuitiveNZ <jasontomasi@gmail.com>
2024-01-08 23:30:25 +1300

    Update FifthElement.lua

--- FifthElement.lua
+++ FifthElement.lua
@@ -1,11 +1,13 @@
 @name Fifth Element
 @inputs B1 B2 B3 B4 B5
-@persist E2POS:vector ELEMENTS:array ELEMENT:entity ROCK1:vector ROCK2:vector ROCK3:vector ROCK4:vector E1 E2 E3 E4 E5 RADIUS
+@persist ELEMENTS:array ELEMENT:entity ROCK1:vector ROCK2:vector ROCK3:vector ROCK4:vector E1 E2 E3 E4 E5 RADIUS
 
-
-interval(500)
+interval(1000)
 
 E5 = E1 + E2 + E3 + E4
+
+E2CHIP = entity()
+E2POS = entity():pos()
 
 
 if (first()) {
@@ -20,7 +22,6 @@ if (first()) {
 	
     E2CHIP = entity()
     E2POS = entity():pos()
-    TORUSPOS = E2POS + vec(0,0,80)
 
     #propSpawn("models/cubic/props/crystal1.mdl", E2POS + vec(0,120,60), 0)
     #propSpawn("models/cubic/props/crystal1.mdl", E2POS + vec(0,-120,60), 0)
@@ -82,95 +83,38 @@ holoColor(50, vec(45,45,45))
     ROCK2 = vec(255,255,0) # YELLOW
     ROCK3 = vec(0,255,0) # GREEN
     ROCK4 = vec(0,77,255) # BLUE
-    
-}
 
 
 
-
-
-Radius = 50
-Speed = 7
-Height = 20
-
-
-
-
-
-
-if(E1 == 1) {
-    lightCreate(1, E2POS + vec(RADIUS,0,90), ROCK1, 150, 200)
-    holoCreate(9)
-    holoPos(9, holoEntity(1):pos() + vec(0,0,25))
-    #holoModel(9, "models/holograms/cube.mdl")
-    holoScale(9, vec(0.8,0.8,7))
-    holoColor(9, ROCK1)
-}
-
-if(E2 == 1) {
-    lightCreate(2, E2POS + vec(-RADIUS,0,90), ROCK2, 150, 200)
-    holoCreate(10)
-    holoPos(10, holoEntity(2):pos() + vec(0,0,35))
-    #holoModel(10, "models/holograms/cube.mdl")
-    holoScale(10, vec(0.8,0.8,7))
-    holoColor(10, ROCK2)
-}
-
-if(E3 == 1) {
-    lightCreate(3, E2POS + vec(0,-RADIUS,90), ROCK3, 150, 200)
-    holoCreate(11)
-    holoPos(11, holoEntity(3):pos() + vec(0,0,25))
-    #holoModel(11, "models/holograms/cube.mdl")
-    holoScale(11, vec(0.8,0.8,7))
-    holoColor(11, ROCK3)
-}
-
-if(E4 == 1) {
-    lightCreate(4, E2POS + vec(0,RADIUS,90), ROCK4, 150, 200)
-    holoCreate(12)
-    holoPos(12, holoEntity(4):pos() + vec(0,0,25))
-    #holoModel(12, "models/holograms/cube.mdl")
-    holoScale(12, vec(0.8,0.8,7))
-    holoColor(12, ROCK4)
-}
-
-
-#lightCreate(1, E2POS + vec(RADIUS,0,90), ROCK1, 150, 200)
-#lightCreate(2, E2POS + vec(-RADIUS,0,90), ROCK2, 150, 200)
-#lightCreate(3, E2POS + vec(0,-RADIUS,90), ROCK3, 150, 200)
-#lightCreate(4, E2POS + vec(0,RADIUS,90), ROCK4, 150, 200)
-
-
-
-if(E5 == 4) {
+function funcActivateLight() {
 
 	holoMaterial(9, "models/alyx/emptool_glow")
 	holoMaterial(10, "models/alyx/emptool_glow")
 	holoMaterial(11, "models/alyx/emptool_glow")
 	holoMaterial(12, "models/alyx/emptool_glow")
 
-# Add extra lights to each stone
-lightCreate(5, E2POS + vec(0,RADIUS,35), ROCK4, 150, 200)
-lightCreate(6, E2POS + vec(0,RADIUS,35), ROCK4, 150, 200)
-lightCreate(7, E2POS + vec(0,RADIUS,35), ROCK4, 150, 200)
-lightCreate(8, E2POS + vec(0,RADIUS,35), ROCK4, 150, 200)
+lightCreate(9, holoEntity(1):pos() + vec(0,0,100), ROCK1, 300, 400)
+lightCreate(10, holoEntity(2):pos() + vec(0,0,100), ROCK2, 300, 400)
+lightCreate(11, holoEntity(3):pos() + vec(0,0,100), ROCK3, 300, 400)
+lightCreate(12, holoEntity(4):pos() + vec(0,0,100), ROCK4, 300, 400)
 
-timer("Activate", 4000)
-}
-
-
-function funcActivateLight() {
-
-    lightCreate(9, E2POS + vec(0,0,100), vec(255,255,255), 200, 200) # E5: WHITE
+    lightCreate(30, E2POS + vec(0,0,100), vec(255,255,255), 200, 200) # E5: WHITE
     
 # TORUS RING
     holoCreate(17)
     holoPos(17, E2POS + vec(0,0,90))
-    #holoAng(17, ChosenGPS:angles())
-    holoScaleUnits(17, vec(170,170,100))
+    holoScaleUnits(17, vec(168,168,98))
     holoModel(17, "hq_torus")
-    holoMaterial(17, "models/props_forest/waterfall001")   # models/props_forest/waterfall001, models/props_c17/fisheyelens_dx60
+    holoMaterial(17, "Models/props_lab/monitorface")   # models/props_forest/waterfall001, models/props_c17/fisheyelens_dx60, Models/props_lab/monitorface, models/props_combine/tpballglow
     holoColor(17, vec(255,255,255), 200)
+# TORUS RING 2
+    #holoCreate(18)
+    #holoPos(18, E2POS + vec(0,0,90))
+    #holoScaleUnits(18, vec(170,170,100))
+    #holoModel(18, "hq_torus")
+    #holoMaterial(18, "models/props_c17/fisheyelens_dx60")   # models/props_forest/waterfall001, models/props_c17/fisheyelens_dx60
+    #holoColor(18, vec(255,0,0), 128)
+    
     
     TORUS = holoEntity(17)
     TORUSPOS = TORUS:pos()
@@ -197,15 +141,121 @@ holoMaterial(Index+10, "models/alyx/emptool_glow")    # models/alyx/emptool_glow
 
 }
 }
-    
-    holoColor(19, vec(255,0,77), 255)
-    holoColor(20, vec(255,255,0), 255)
-    holoColor(21, vec(0,255,0), 255)
-    holoColor(22, vec(0,213,255), 255)
- 
-    timer("DivineSequence", 1000)
 
+    holoColor(19, ROCK1, 255)
+    holoColor(20, ROCK2, 255)
+    holoColor(21, ROCK3, 255)
+    holoColor(22, ROCK4, 255)
+
+   timer("DivineSequence", 2000)
+
+} #ENDFUNC
+
+
+function funcDivineSequence() {
+    
+    E1 = 0
+    E2 = 0
+    E3 = 0
+    E4 = 0
+    E5 = 0
+    
+    lightCreate(31, E2POS + vec(0,0,150), vec(255,255,255), 700, 700)
+    
+    # Turn the statue white during divine light seq
+    holoColor(50, vec(255,255,255))
+    
+    # Column of divine light
+    holoCreate(49)
+    holoModel(49, "cylinder")
+    holoPos(49, E2POS + vec(0,0,4096)) #4096
+    holoScaleUnits(49, vec(170,170,8192))  # 8092
+    holoMaterial(49, "models/alyx/emptool_glow") 
+    holoColor(49, vec(255,255,255))
+    
+    holoCreate(48)
+    holoModel(48, "cylinder")
+    holoPos(48, E2POS + vec(0,0,4096)) #4096
+    holoAng(48, ang(0,-90,0))
+    holoScaleUnits(48, vec(168,168,8192))  # 8092
+    holoMaterial(48, "models/props_forest/waterfall001") 
+    holoColor(48, vec(255,255,255))
+	
+	timer("Cleanup1",13000)
+ 
+} #ENDFUNC
+
+} # END FIRST
+
+
+
+
+
+if(E1 == 1) {
+    lightCreate(1, E2POS + vec(RADIUS,0,100), ROCK1, 150, 200)
+    holoCreate(9)
+    holoPos(9, holoEntity(1):pos() + vec(0,0,25))
+    holoScale(9, vec(0.8,0.8,7))
+    holoColor(9, ROCK1)
+    
+    lightCreate(81, holoEntity(5):pos() + vec(0,-35,25), ROCK1, 45, 20)
+    lightCreate(82, holoEntity(5):pos() + vec(-35,0,25), ROCK1, 45, 20)
+    lightCreate(83, holoEntity(5):pos() + vec(0,35,25), ROCK1, 45, 20)
+    lightCreate(84, holoEntity(5):pos() + vec(35,0,25), ROCK1, 45, 20)
 }
+
+if(E2 == 1) {
+    lightCreate(2, E2POS + vec(-RADIUS,0,100), ROCK2, 150, 200)
+    holoCreate(10)
+    holoPos(10, holoEntity(2):pos() + vec(0,0,25))
+    holoScale(10, vec(0.8,0.8,7))
+    holoColor(10, ROCK2)
+    
+    lightCreate(85, holoEntity(6):pos() + vec(0,-35,25), ROCK2, 45, 20)
+    lightCreate(86, holoEntity(6):pos() + vec(-35,0,25), ROCK2, 45, 20)
+    lightCreate(87, holoEntity(6):pos() + vec(0,35,25), ROCK2, 45, 20)
+    lightCreate(88, holoEntity(6):pos() + vec(35,0,25), ROCK2, 45, 20)
+}
+
+if(E3 == 1) {
+    lightCreate(3, E2POS + vec(0,-RADIUS,100), ROCK3, 150, 200)
+    holoCreate(11)
+    holoPos(11, holoEntity(3):pos() + vec(0,0,25))
+    holoScale(11, vec(0.8,0.8,7))
+    holoColor(11, ROCK3)
+    
+    lightCreate(89, holoEntity(7):pos() + vec(0,-35,25), ROCK3, 45, 20)
+    lightCreate(90, holoEntity(7):pos() + vec(-35,0,25), ROCK3, 45, 20)
+    lightCreate(91, holoEntity(7):pos() + vec(0,35,25), ROCK3, 45, 20)
+    lightCreate(92, holoEntity(7):pos() + vec(35,0,25), ROCK3, 45, 20)
+}
+
+if(E4 == 1) {
+    lightCreate(4, E2POS + vec(0,RADIUS,100), ROCK4, 150, 200)
+    holoCreate(12)
+    holoPos(12, holoEntity(4):pos() + vec(0,0,25))
+    holoScale(12, vec(0.8,0.8,7))
+    holoColor(12, ROCK4)
+    
+    lightCreate(93, holoEntity(8):pos() + vec(0,-35,25), ROCK4, 45, 20)
+    lightCreate(94, holoEntity(8):pos() + vec(-35,0,25), ROCK4, 45, 20)
+    lightCreate(95, holoEntity(8):pos() + vec(0,35,25), ROCK4, 45, 20)
+    lightCreate(96, holoEntity(8):pos() + vec(35,0,25), ROCK4, 45, 20)
+}
+
+if(E5 == 4) {
+
+# Add extra lights to each stone
+#lightCreate(5, holoEntity(1):pos() + vec(0,0,50), ROCK1, 200, 300)
+#lightCreate(6, holoEntity(2):pos() + vec(0,0,50), ROCK2, 200, 300)
+#lightCreate(7, holoEntity(3):pos() + vec(0,0,50), ROCK3, 200, 300)
+#lightCreate(8, holoEntity(4):pos() + vec(0,0,50), ROCK4, 200, 300)
+
+timer("Activate", 4000)
+}
+
+
+
 
 
 
@@ -240,35 +290,7 @@ if(clk("Activate")) {
 
 
 if(clk("DivineSequence")) {
-    
-    E1 = 0
-    E2 = 0
-    E3 = 0
-    E4 = 0
-    E5 = 0
-    
-    lightCreate(10, E2POS + vec(0,0,150), vec(255,255,255), 700, 700) # E5: WHITE
-    
-    # Turn the statue white during divine light seq
-    holoColor(50, vec(255,255,255))
-    
-    # Column of divine light
-    holoCreate(49)
-    holoModel(49, "cylinder")
-    holoPos(49, E2POS + vec(0,0,4096)) #4096
-    holoScaleUnits(49, vec(170,170,8192))  # 8092
-    holoMaterial(49, "models/alyx/emptool_glow") 
-    holoColor(49, vec(255,255,255))
-    
-    holoCreate(48)
-    holoModel(48, "cylinder")
-    holoPos(48, E2POS + vec(0,0,4096)) #4096
-    holoAng(48, ang(0,-90,0))
-    holoScaleUnits(48, vec(168,168,8192))  # 8092
-    holoMaterial(48, "models/props_forest/waterfall001") 
-    holoColor(48, vec(255,255,255))
-	
-	timer("Cleanup1",9000)
+    funcDivineSequence()
 }
 
 
@@ -281,8 +303,10 @@ if(clk("Cleanup1")) {
 	
     holoColor(50, vec(45,45,45))
     
+    #TORUS
     holoDelete(17)
     
+    #COLUMN
     holoDelete(49)
     holoDelete(48)
     
@@ -292,13 +316,13 @@ for (Index = 13, 22) {
     }
 }
 
-for (Index = 5, 22) {
+for (Index = 5, 32) {
     if (lightEntity(Index)) {
         lightRemove(Index)
     }
 }    
 	
-timer("Cleanup2",4000)
+timer("Cleanup2",6000)
 }
 
 
@@ -307,6 +331,12 @@ if(clk("Cleanup2")) {
 for (Index = 9, 12) {
     if (holoEntity(Index)) {
         holoDelete(Index)
+    }
+}
+
+for (Index = 80, 100) {
+    if (lightEntity(Index)) {
+        lightRemove(Index)
     }
 }
 

e55da0989c2f86745b6c2a16a2531b07c60ebd86
IntuitiveNZ <jasontomasi@gmail.com>
2024-01-09 01:06:25 +1300

    Update FifthElement.lua

--- FifthElement.lua
+++ FifthElement.lua
@@ -54,22 +54,22 @@ holoScale(4, vec(0.75,0.75,0.55))
 holoCreate(5)
 holoPos(5, holoEntity(1):pos() + vec(0,0,20))
 holoMaterial(5, "models/props_c17/furniturefabric001a")
-holoScale(5, vec(0.9,0.9,5))
+holoScale(5, vec(0.8,0.8,5))
 
 holoCreate(6)
 holoPos(6, holoEntity(2):pos() + vec(0,0,20))
 holoMaterial(6, "models/props_c17/furniturefabric001a")
-holoScale(6, vec(0.9,0.9,5))
+holoScale(6, vec(0.8,0.8,5))
 
 holoCreate(7)
 holoPos(7, holoEntity(3):pos() + vec(0,0,20))
 holoMaterial(7, "models/props_c17/furniturefabric001a")
-holoScale(7, vec(0.9,0.9,5))
+holoScale(7, vec(0.8,0.8,5))
 
 holoCreate(8)
 holoPos(8, holoEntity(4):pos() + vec(0,0,20))
 holoMaterial(8, "models/props_c17/furniturefabric001a")
-holoScale(8, vec(0.9,0.9,5))
+holoScale(8, vec(0.98,0.8,5))
 
 
 holoCreate(50)
@@ -79,10 +79,10 @@ holoScale(50, vec(0.55,0.55,0.55))
 holoColor(50, vec(45,45,45))
 
     # COLOURS
-    ROCK1 = vec(255,0,77) # PINK
+    ROCK1 = vec(132,0,50) # PINK
     ROCK2 = vec(255,255,0) # YELLOW
     ROCK3 = vec(0,255,0) # GREEN
-    ROCK4 = vec(0,77,255) # BLUE
+    ROCK4 = vec(0,161,255) # BLUE
 
 
 
@@ -93,10 +93,10 @@ function funcActivateLight() {
 	holoMaterial(11, "models/alyx/emptool_glow")
 	holoMaterial(12, "models/alyx/emptool_glow")
 
-lightCreate(9, holoEntity(1):pos() + vec(0,0,100), ROCK1, 300, 400)
-lightCreate(10, holoEntity(2):pos() + vec(0,0,100), ROCK2, 300, 400)
-lightCreate(11, holoEntity(3):pos() + vec(0,0,100), ROCK3, 300, 400)
-lightCreate(12, holoEntity(4):pos() + vec(0,0,100), ROCK4, 300, 400)
+lightCreate(9, holoEntity(1):pos() + vec(0,0,90), ROCK1, 150, 150)
+lightCreate(10, holoEntity(2):pos() + vec(0,0,90), ROCK2, 150, 150)
+lightCreate(11, holoEntity(3):pos() + vec(0,0,90), ROCK3, 150, 150)
+lightCreate(12, holoEntity(4):pos() + vec(0,0,90), ROCK4, 150, 150)
 
     lightCreate(30, E2POS + vec(0,0,100), vec(255,255,255), 200, 200) # E5: WHITE
     
@@ -105,7 +105,7 @@ lightCreate(12, holoEntity(4):pos() + vec(0,0,100), ROCK4, 300, 400)
     holoPos(17, E2POS + vec(0,0,90))
     holoScaleUnits(17, vec(168,168,98))
     holoModel(17, "hq_torus")
-    holoMaterial(17, "Models/props_lab/monitorface")   # models/props_forest/waterfall001, models/props_c17/fisheyelens_dx60, Models/props_lab/monitorface, models/props_combine/tpballglow
+    holoMaterial(17, "models/spawn_effect2")   # models/props_forest/waterfall001, models/props_c17/fisheyelens_dx60, Models/props_lab/monitorface, models/props_combine/tpballglow, Effects/fake_fog01
     holoColor(17, vec(255,255,255), 200)
 # TORUS RING 2
     #holoCreate(18)
@@ -147,7 +147,7 @@ holoMaterial(Index+10, "models/alyx/emptool_glow")    # models/alyx/emptool_glow
     holoColor(21, ROCK3, 255)
     holoColor(22, ROCK4, 255)
 
-   timer("DivineSequence", 2000)
+   timer("DivineSequence", 1000)
 
 } #ENDFUNC
 
@@ -192,55 +192,55 @@ function funcDivineSequence() {
 
 
 if(E1 == 1) {
-    lightCreate(1, E2POS + vec(RADIUS,0,100), ROCK1, 150, 200)
+    #lightCreate(1, E2POS + vec(RADIUS,0,100), ROCK1, 150, 200)
     holoCreate(9)
     holoPos(9, holoEntity(1):pos() + vec(0,0,25))
-    holoScale(9, vec(0.8,0.8,7))
+    holoScale(9, vec(0.7,0.7,7))
     holoColor(9, ROCK1)
     
-    lightCreate(81, holoEntity(5):pos() + vec(0,-35,25), ROCK1, 45, 20)
-    lightCreate(82, holoEntity(5):pos() + vec(-35,0,25), ROCK1, 45, 20)
-    lightCreate(83, holoEntity(5):pos() + vec(0,35,25), ROCK1, 45, 20)
-    lightCreate(84, holoEntity(5):pos() + vec(35,0,25), ROCK1, 45, 20)
+    lightCreate(81, holoEntity(5):pos() + vec(0,-35,25), ROCK1, 50, 15)
+    lightCreate(82, holoEntity(5):pos() + vec(-35,0,25), ROCK1, 50, 15)
+    lightCreate(83, holoEntity(5):pos() + vec(0,35,25), ROCK1, 50, 15)
+    #lightCreate(84, holoEntity(5):pos() + vec(35,0,25), ROCK1, 50, 15)
 }
 
 if(E2 == 1) {
-    lightCreate(2, E2POS + vec(-RADIUS,0,100), ROCK2, 150, 200)
+    #lightCreate(2, E2POS + vec(-RADIUS,0,100), ROCK2, 150, 200)
     holoCreate(10)
     holoPos(10, holoEntity(2):pos() + vec(0,0,25))
-    holoScale(10, vec(0.8,0.8,7))
+    holoScale(10, vec(0.7,0.7,7))
     holoColor(10, ROCK2)
     
-    lightCreate(85, holoEntity(6):pos() + vec(0,-35,25), ROCK2, 45, 20)
-    lightCreate(86, holoEntity(6):pos() + vec(-35,0,25), ROCK2, 45, 20)
-    lightCreate(87, holoEntity(6):pos() + vec(0,35,25), ROCK2, 45, 20)
-    lightCreate(88, holoEntity(6):pos() + vec(35,0,25), ROCK2, 45, 20)
+    lightCreate(85, holoEntity(6):pos() + vec(0,-35,25), ROCK2, 50, 15)
+    lightCreate(86, holoEntity(6):pos() + vec(-35,0,25), ROCK2, 50, 15)
+    lightCreate(87, holoEntity(6):pos() + vec(0,35,25), ROCK2, 50, 15)
+    #lightCreate(88, holoEntity(6):pos() + vec(35,0,25), ROCK2, 50, 15)
 }
 
 if(E3 == 1) {
-    lightCreate(3, E2POS + vec(0,-RADIUS,100), ROCK3, 150, 200)
+    #lightCreate(3, E2POS + vec(0,-RADIUS,100), ROCK3, 150, 200)
     holoCreate(11)
     holoPos(11, holoEntity(3):pos() + vec(0,0,25))
-    holoScale(11, vec(0.8,0.8,7))
+    holoScale(11, vec(0.7,0.7,7))
     holoColor(11, ROCK3)
     
-    lightCreate(89, holoEntity(7):pos() + vec(0,-35,25), ROCK3, 45, 20)
-    lightCreate(90, holoEntity(7):pos() + vec(-35,0,25), ROCK3, 45, 20)
-    lightCreate(91, holoEntity(7):pos() + vec(0,35,25), ROCK3, 45, 20)
-    lightCreate(92, holoEntity(7):pos() + vec(35,0,25), ROCK3, 45, 20)
+    lightCreate(89, holoEntity(7):pos() + vec(0,-35,25), ROCK3, 50, 15)
+    lightCreate(90, holoEntity(7):pos() + vec(-35,0,25), ROCK3, 50, 15)
+    lightCreate(91, holoEntity(7):pos() + vec(0,35,25), ROCK3, 50, 15)
+    #lightCreate(92, holoEntity(7):pos() + vec(35,0,25), ROCK3, 50, 15)
 }
 
 if(E4 == 1) {
-    lightCreate(4, E2POS + vec(0,RADIUS,100), ROCK4, 150, 200)
+    #lightCreate(4, E2POS + vec(0,RADIUS,100), ROCK4, 150, 200)
     holoCreate(12)
     holoPos(12, holoEntity(4):pos() + vec(0,0,25))
-    holoScale(12, vec(0.8,0.8,7))
+    holoScale(12, vec(0.7,0.7,7))
     holoColor(12, ROCK4)
     
-    lightCreate(93, holoEntity(8):pos() + vec(0,-35,25), ROCK4, 45, 20)
-    lightCreate(94, holoEntity(8):pos() + vec(-35,0,25), ROCK4, 45, 20)
-    lightCreate(95, holoEntity(8):pos() + vec(0,35,25), ROCK4, 45, 20)
-    lightCreate(96, holoEntity(8):pos() + vec(35,0,25), ROCK4, 45, 20)
+    lightCreate(93, holoEntity(8):pos() + vec(0,-35,25), ROCK4, 50, 15)
+    lightCreate(94, holoEntity(8):pos() + vec(-35,0,25), ROCK4, 50, 15)
+    lightCreate(95, holoEntity(8):pos() + vec(0,35,25), ROCK4, 50, 15)
+    #lightCreate(96, holoEntity(8):pos() + vec(35,0,25), ROCK4, 50, 15)
 }
 
 if(E5 == 4) {
@@ -322,7 +322,7 @@ for (Index = 5, 32) {
     }
 }    
 	
-timer("Cleanup2",6000)
+timer("Cleanup2",3000)
 }
 
 
